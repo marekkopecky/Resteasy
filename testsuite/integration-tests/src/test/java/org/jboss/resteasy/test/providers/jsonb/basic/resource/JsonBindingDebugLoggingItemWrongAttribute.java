0d5c3f37936b2c86054a10061680ffd1fb47498b
Marek Kopecky <mkopecky@redhat.com>
2019-01-15 08:14:27 +0100

    [RESTEASY-2016] Tests for JsonBindingProvider logging

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jsonb/basic/resource/JsonBindingDebugLoggingItemWrongAttribute.java
@@ -0,0 +1,13 @@
+package org.jboss.resteasy.test.providers.jsonb.basic.resource;
+
+public class JsonBindingDebugLoggingItemWrongAttribute {
+    String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
